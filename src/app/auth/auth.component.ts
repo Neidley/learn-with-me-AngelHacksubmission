68f855b810269588c6dfad14481ef802a6a387da
Andrew Anderson <Anderson7301@icloud.com>
2018-07-14 12:46:22 -0500

    initial auth services, comp, routing for landing as well

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth.service';
+import { Router } from '../../../node_modules/@angular/router';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css']
+})
+export class AuthComponent implements OnInit {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.authService.user.subscribe(user => {
+      if (user) this.router.navigateByUrl('home');
+    })
+  }
+
+  ngOnInit() {
+  }
+
+
+  login(email: string, password: string) {
+    this.authService.emailPasswordLogin(email, password).then(response => {
+      console.log(response);
+      this.router.navigateByUrl('home');
+    });
+  }
+
+  signup() {
+    this.authService.emailPasswordSignUp('', '').then(response => console.log(response))
+  }
+
+}
